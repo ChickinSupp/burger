55fd954f1bc3fc9fe85652815bf394f01bb451b1
David Baker <daviiddamiianbaker@gmail.com>
2018-11-26 18:57:06 -0500

    creating orm

---
+++ config/orm.js
@@ -0,0 +1,9 @@
+let connection = require('../config/connection.js');
+function printQuestionMarks(num) {
+    let arr = [];
+
+    for (let i=0; i<num; i++){
+        arr.push('?');
+    }
+    return arr.toString();
+};
